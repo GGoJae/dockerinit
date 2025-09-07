2a6268f1772f49e9c332ea2f14cad77cad01a1e4
GGojae <woajrsla100@gmail.com>
2025-09-07 10:15:12 +0900

    refactor: 임포트 잘 못 돼있는것들 수정
    # Conflicts:
    #	backend/dockerinit/src/main/java/com/dockerinit/features/support/validation/composeService/ExactlyOneOfImageOrBuildValidator.java

--- backend/dockerinit/src/main/java/com/dockerinit/features/support/validation/composeService/ExactlyOneOfImageOrBuildValidator.java
+++ backend/dockerinit/src/main/java/com/dockerinit/features/support/validation/composeService/ExactlyOneOfImageOrBuildValidator.java
@@ -1,12 +1,8 @@
 package com.dockerinit.features.support.validation.composeService;
 
-import com.dockerinit.features.dockercompose.domain.Service;
-import com.dockerinit.features.dockercompose.dto.request.ComposeRequestV1;
 import com.dockerinit.features.dockercompose.dto.spec.ServiceSpecDTO;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-
-import java.util.Objects;
 
 public class ExactlyOneOfImageOrBuildValidator implements ConstraintValidator<ExactlyOneOfImageOrBuild, ServiceSpecDTO> {
 
